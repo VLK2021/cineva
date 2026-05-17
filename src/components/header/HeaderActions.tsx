5e1a907d3afc3452edfa973e5534b09c6f1bb3f1
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-05-17 20:28:32 +0300

    movies

--- src/components/header/HeaderActions.tsx
+++ src/components/header/HeaderActions.tsx
@@ -16,36 +16,36 @@ const HeaderActions = () => {
             <LangSwitcher />
             <ThemeSwitcher />
 
-            <button
-                type="button"
-                className="
-                    hidden
-                    h-8
-                    items-center
-                    justify-center
-                    rounded-full
-                    border
-                    border-[var(--color-border)]
-                    bg-[var(--color-card)]
-                    px-3
-                    text-xs
-                    font-semibold
-                    text-[var(--color-text)]
-                    shadow-sm
-                    transition-all
-                    duration-300
-                    hover:border-[var(--color-brand)]
-                    hover:bg-[var(--color-brand)]
-                    hover:text-white
-                    active:scale-95
-                    sm:inline-flex
-                    md:h-10
-                    md:px-4
-                    md:text-sm
-                "
-            >
-                Log in
-            </button>
+            {/*<button*/}
+            {/*    type="button"*/}
+            {/*    className="*/}
+            {/*        hidden*/}
+            {/*        h-8*/}
+            {/*        items-center*/}
+            {/*        justify-center*/}
+            {/*        rounded-full*/}
+            {/*        border*/}
+            {/*        border-[var(--color-border)]*/}
+            {/*        bg-[var(--color-card)]*/}
+            {/*        px-3*/}
+            {/*        text-xs*/}
+            {/*        font-semibold*/}
+            {/*        text-[var(--color-text)]*/}
+            {/*        shadow-sm*/}
+            {/*        transition-all*/}
+            {/*        duration-300*/}
+            {/*        hover:border-[var(--color-brand)]*/}
+            {/*        hover:bg-[var(--color-brand)]*/}
+            {/*        hover:text-white*/}
+            {/*        active:scale-95*/}
+            {/*        sm:inline-flex*/}
+            {/*        md:h-10*/}
+            {/*        md:px-4*/}
+            {/*        md:text-sm*/}
+            {/*    "*/}
+            {/*>*/}
+            {/*    Log in*/}
+            {/*</button>*/}
         </div>
     );
 };
