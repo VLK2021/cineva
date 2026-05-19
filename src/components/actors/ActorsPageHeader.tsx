1b10f2f0d35dbcd9811c4f05968510247aaa41d0
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-05-19 23:21:42 +0300

    commit

--- src/components/actors/ActorsPageHeader.tsx
+++ src/components/actors/ActorsPageHeader.tsx
@@ -7,10 +7,6 @@ type ActorsPageHeaderProps = {
 const ActorsPageHeader = ({ sort }: ActorsPageHeaderProps) => {
     return (
         <div className="mb-8">
-            <p className="mb-2 text-sm font-bold uppercase tracking-wide text-[var(--color-brand)]">
-                Каталог
-            </p>
-
             <ActorsFiltersBar sort={sort} />
         </div>
     );
