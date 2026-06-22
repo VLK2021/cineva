1e1fd7394e746acdb07e200699d6584650eca338
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-06-22 14:08:13 +0300

    ceo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,9 +2,31 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: "*",
-            disallow: "/",
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+            {
+                userAgent: "GPTBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "CCBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "ClaudeBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "Bytespider",
+                disallow: "/",
+            },
+            {
+                userAgent: "Amazonbot",
+                disallow: "/",
+            },
+        ],
     };
 }
